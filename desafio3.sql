dceef812abc8f2f2bd51d9d4e113dcf891dbe4c2
matheusmendes <matheus_jp08@hotmail.com>
2025-03-23 16:34:35 -0300

    Desafio 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,3 +1,9 @@
 SELECT  prod.nome, prod.preco, forn.nome
 FROM    produto prod
-INNER JOIN fornecedores forn
+INNER JOIN fornecedores forn ON prod.fornecedores_id = forn.id
+
+SELECT  cli.nome, cli.telefone, vende.nome, form.forma_pagamento
+FROM    cliente cli
+INNER JOIN venda vend ON cli.id = vend.cliente_id
+INNER JOIN vendedor vende ON vende.id = vend.vendedor_id
+INNER JOIN pagamento pag ON pag.id = vend.pagamento_id
